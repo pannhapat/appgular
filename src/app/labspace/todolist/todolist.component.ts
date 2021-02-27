1b2ecb6b32a5a965c29662e0ef10d93ce54178e3
devdodone <devdodone@gmail.com>
2021-02-27 22:32:14 +0700

    todo

--- src/app/labspace/todolist/todolist.component.ts
+++ src/app/labspace/todolist/todolist.component.ts
@@ -1,33 +1,48 @@
 import { Component ,Input,Output, EventEmitter } from '@angular/core';
 
 
+
+interface ITask {
+  name?: string;
+}
+
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent  {
-  items = ['Task 1', 'Task 2', 'Task 3', 'Task 4'];
+  // items = ['Task 1', 'Task 2', 'Task 3', 'Task 4'];
+
+  items: ITask[] = [{ name: "Task 1" }, { name: "Task 2" }, { name: "Task 3" }, {name:"Task 4"}];
 
   constructor() {
 
   }
   @Input()
-  item = "task";
+
 
   indexitem = 0;
   currentItem = 'Television';
 
-  @Output() newItemEvent = new EventEmitter<string>();
-  @Output() deleteItemEvent = new EventEmitter<number>();
-  addNewItem(value: string) {
-    this.newItemEvent.emit(value);
-    this.items.push(value);
+  @Output() newItemEvent = new EventEmitter<ITask>();
+  @Output() deleteItemEvent = new EventEmitter<ITask>();
+
+  addNewItem(task: ITask) {
+    this.newItemEvent.emit(task);
+    this.items.push(task);
 
   }
-  deleteItem(value: number) {
-    this.deleteItemEvent.emit(value);
-    this.items.splice(value, 1);
+  deleteItem(task: ITask) {
+    this.deleteItemEvent.emit(task);
+    const index: number = this.items.indexOf(task);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+  }
+
+
+
+    // this.items.splice(task, 1);
 
   }
 
