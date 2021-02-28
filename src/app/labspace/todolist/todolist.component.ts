f8c4b9f1336b7910647a5de033497823165547ae
devdodone <devdodone@gmail.com>
2021-02-28 14:42:52 +0700

    Todo

--- src/app/labspace/todolist/todolist.component.ts
+++ src/app/labspace/todolist/todolist.component.ts
@@ -1,9 +1,10 @@
-import { Component ,Input,Output, EventEmitter } from '@angular/core';
+import { Component ,Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../models/task';
 
 
-interface ITask {
-  name?: string;
-}
+// interface ITask {
+//   name?: string;
+// }
 
 @Component({
   selector: 'app-todolist',
@@ -11,8 +12,9 @@ interface ITask {
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent  {
-
-  items: ITask[] = [{ name: "Task 1" }, { name: "Task 2" }, { name: "Task 3" }, {name:"Task 4"}];
+  // แบบแรก
+  // items: ITask[] = [{ name: "Task 1" }, { name: "Task 2" }, { name: "Task 3" }, {name:"Task 4"}];
+  items: Task[] = [{ id:1, name: "Task 1", description:"Description Task 1" }, { id:2, name: "Task 2", description:"Description Task 2" }, { id:3, name: "Task 3", description:"Description Task 3" }, {id:4, name: "Task 4", description:"Description Task 4"}];
 
   constructor() {
 
@@ -23,15 +25,22 @@ export class TodolistComponent  {
   indexitem = 0;
   currentItem = 'Television';
 
-  @Output() newItemEvent = new EventEmitter<ITask>();
-  @Output() deleteItemEvent = new EventEmitter<ITask>();
+  @Output() newItemEvent = new EventEmitter<Task>();
+  @Output() deleteItemEvent = new EventEmitter<Task>();
 
-  addNewItem(task: ITask) {
+  showingTask = this.items[0];
+
+  showTask(task: Task) {
+    this.showingTask = task;
+  }
+
+  addNewItem(task: Task) {
+    task.id = this.items.length + 1 ;
     this.newItemEvent.emit(task);
     this.items.push(task);
 
   }
-  deleteItem(task: ITask) {
+  deleteItem(task: Task) {
     this.deleteItemEvent.emit(task);
     const index: number = this.items.indexOf(task);
     if (index !== -1) {
@@ -39,6 +48,24 @@ export class TodolistComponent  {
     }
 
   }
+
+
+  // @Output() newItemEvent = new EventEmitter<ITask>();
+  // @Output() deleteItemEvent = new EventEmitter<ITask>();
+
+  // addNewItem(task: ITask) {
+  //   this.newItemEvent.emit(task);
+  //   this.items.push(task);
+
+  // }
+  // deleteItem(task: ITask) {
+  //   this.deleteItemEvent.emit(task);
+  //   const index: number = this.items.indexOf(task);
+  //   if (index !== -1) {
+  //     this.items.splice(index, 1);
+  //   }
+
+  // }
 
 
 
